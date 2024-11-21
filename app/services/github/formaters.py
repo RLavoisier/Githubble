4e8e0a3d3caca3021dac253378e33a243409217d
lavoisierrenaud <renaud.lavoisier@gmail.com>
2024-11-21 04:11:50 +0100

    Using orjson for better response parsing

--- app/services/github/formaters.py
+++ app/services/github/formaters.py
@@ -1,12 +1,13 @@
 from abc import ABC, abstractmethod
 from typing import Any
 
+import orjson
 from httpx import Response
 
 
 class GithubResponseFormatter(ABC):
     async def __call__(self, response: Response) -> Any:
-        return await self._format_json_resonse(response.json())
+        return await self._format_json_resonse(orjson.loads(response.content))
 
     @abstractmethod
     async def _format_json_resonse(self, response_json: list[dict[str, Any]]) -> Any:
